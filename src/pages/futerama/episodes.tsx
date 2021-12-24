f09783a84b06323345b68a9c6bd2ced38cefe5d3
gyuri <jae04099@catholic.ac.kr>
2021-12-24 23:38:56 +0900

    :lipstick: styled episodes component

--- src/pages/futerama/episodes.tsx
+++ src/pages/futerama/episodes.tsx
@@ -1,29 +1,75 @@
 import type { NextPage } from "next";
+import styled from "styled-components";
 import { useApi } from "../../hooks/useApi";
 import { TYPES } from "../../constants";
-import {Episodes} from '../../types/episodes';
-import {Error, Loading} from "../../components/index";
+import { Episodes } from "../../types/episodes";
+import { Error, Loading } from "../../components/index";
 
 const EpisodesPage: NextPage = () => {
-    const {data, error} = useApi(TYPES[2]);
-    if(error) return <Error />
-    if(!data) return <Loading />
+    const { data, error } = useApi(TYPES[2]);
+    if (error) return <Error />;
+    if (!data) return <Loading />;
     return (
-        <div>
+        <Container>
+            <h1>EPISODES</h1>
             {data.map((epiData: Episodes) => {
                 return (
-                    <div key={epiData.id}>
+                    <div className="card-wrap" key={epiData.id}>
                         <ul>
-                        <li>title: {epiData.title}</li>
-                        <li>writers: {epiData.writers}</li>
-                        <li>original air date: {epiData.originalAirDate}</li>
-                        <li>description: {epiData.desc}</li>
+                            <li className="title">{epiData.title}</li>
+                            <li className="writers">Writers: {epiData.writers}</li>
+                            <li className="year">
+                                {epiData.originalAirDate}
+                            </li>
+                            <li className="desc">
+                                description: {epiData.desc}
+                            </li>
                         </ul>
                     </div>
-                )
+                );
             })}
-        </div>
-    )
-}
+        </Container>
+    );
+};
 
 export default EpisodesPage;
+
+const Container = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    h1 {
+        font-size: 64px;
+    }
+    .card-wrap {
+        width: 300px;
+        ul {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            padding: 15px;
+            margin: 15px;
+            border: 1px solid #222;
+            .title {
+                margin-bottom: 24px;
+                font-size: 24px;
+            }
+            .writers {
+                font-size: 20px;
+                padding-bottom: 15px
+            }
+            .year {
+                margin-bottom: 15px;
+            }
+            .desc {
+                display: -webkit-box;
+                max-width: 200px;
+                -webkit-line-clamp: 4;
+                -webkit-box-orient: vertical;
+                overflow: hidden;
+            }
+            li {
+                list-style: none;
+            }
+        }
+    }
+`;

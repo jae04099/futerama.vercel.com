e44db5875deed8cffcefb90e89fa8b23ff776d85
gyurilee <jae04099@gmail.com>
2021-12-27 09:07:08 +0900

    :ambulance: change route path

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const API_ENDPOINT: string = "https://api.sampleapis.com/futurama/"
 
-export const TYPES = ['cast', 'characters', 'episodes', 'info', 'inventory', 'questions']
+export const TYPES = ['cast', 'character', 'episodes', 'info', 'inventory', 'questions']
 
 export const ROUTES_ENDPOINT: string = "https://futerama-vercel-com.vercel.app/futurama/" 
 export const MAIN_ENDPOINT: string = "https://futerama-vercel-com.vercel.app/"
